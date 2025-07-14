ca6284ca6eb9c990e81cdebef63a0f665ca557b8
Miss Maggie <magdalinemutave001@gmail.com>
2025-07-14 21:06:19 +0300

    Component update

--- poster-maker/frontend/src/components/ui/tooltip.jsx
+++ poster-maker/frontend/src/components/ui/tooltip.jsx
@@ -1,13 +1,9 @@
 import * as React from "react"
-// import { Tooltip } from '@radix-ui/react-tooltip';
-import { Tooltip } from '@radix-ui/react-tooltip';
-
+import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 import { cn } from "@/lib/utils"
 
 const TooltipProvider = TooltipPrimitive.Provider
-
-const Tooltip = TooltipPrimitive.Root
-
+const TooltipRoot = TooltipPrimitive.Root
 const TooltipTrigger = TooltipPrimitive.Trigger
 
 const TooltipContent = React.forwardRef((
@@ -24,4 +20,9 @@ const TooltipContent = React.forwardRef((
 ))
 TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } 
+export {
+  TooltipRoot,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider
+}
